29e176f2b8ad313c034cb709d7530323c165db4b
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-24 19:31:17 +0530

    header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import React, { useState, useEffect } from 'react';
+import { Layout } from '../components';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout >
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
