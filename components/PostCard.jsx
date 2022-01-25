076e6a66b0641985738d0cb299f208dfb977ff4c
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-25 18:36:06 +0530

    bug

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 function PostCard({ post }) {
     return (
-        <div className='shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 custom-bg'>
+        <div className='shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-12 custom-bg'>
             <div className="relative overflow-hidden shadow-md pb-80 mb-6">
                 <Image src={post.featuredImage.url} alt={post.title} className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg" layout='fill' />
             </div>
