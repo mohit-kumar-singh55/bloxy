c0bf0a5ee584b2ae357a4b2fee3817579e9b5ca0
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-26 02:11:44 +0530

    Created Comment form

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+// import React, { useState, useEffect, useRef } from 'react';
 
-function Comments() {
+function Comments({ slug }) {
+    // const [error, setError] = useState(false);
+    // const [localStorage, setLocalStorage] = useState(null);
+    // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    // const commentEl = useRef();
+    // const nameEl = useRef();
+    // const emailEl = useRef();
+    // const storeDataEl = useRef();
+
     return (
-        <div>
+        <div className='custom-bg shadow-lg rounded-lg p-8 pb-12 mb-8'>
             <h1>Comments</h1>
         </div>
     );
