d54ab077e510db0fed1023d48c3f691a09ab88e8
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-26 19:41:04 +0530

    comments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,19 +1,39 @@
-// import React, { useState, useEffect, useRef } from 'react';
+import moment from 'moment';
+import React, { useState, useEffect } from 'react';
+import { getComments } from '../services';
+import parse from "html-react-parser";
 
 function Comments({ slug }) {
-    // const [error, setError] = useState(false);
-    // const [localStorage, setLocalStorage] = useState(null);
-    // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [comments, setComments] = useState([]);
 
-    // const commentEl = useRef();
-    // const nameEl = useRef();
-    // const emailEl = useRef();
-    // const storeDataEl = useRef();
+    useEffect(() => {
+        getComments(slug).then((result) => setComments(result));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
 
     return (
-        <div className='custom-bg shadow-lg rounded-lg p-8 pb-12 mb-8'>
-            <h1>Comments</h1>
-        </div>
+        <>
+            {comments.length > 0 && (
+                <div className='custom-bg shadow-lg rounded-lg p-8 pb-12 mb-8'>
+                    <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+                        {comments.length}{' '} Comments
+                    </h3>
+                    {comments.map((comment) => (
+                        <div key={comment.createdAt} className="border-b border-gray-100 mb-4 pb-4">
+                            <p className="mb-4">
+                                <span className='font-semibold'>{comment.name}</span>
+                                {' '}
+                                on
+                                {' '}
+                                {moment(comment.createdAt).format("DD MMM YYYY")}
+                            </p>
+                            <p className="whitespace-pre-line text-gray-600 w-full">{parse(comment.comment)}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
     );
 }
 
