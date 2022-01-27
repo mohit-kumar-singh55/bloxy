83b3156902a677a6be3022992d62fd4cfb10979b
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-27 18:54:22 +0530

    seo

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -26,7 +26,7 @@ function PostCard({ post }) {
                     <span className="align-middle">{moment(post.createdAt).format('DD MMM YYYY')}</span>
                 </div>
             </div>
-            <p className="text-center text-lg font-normal px-4 lg:px-20 mb-8">{post.excerpt}</p>
+            <article className="text-center text-lg font-normal px-4 lg:px-20 mb-8">{post.excerpt}</article>
             <div className="text-center">
                 <Link href={`/post/${post.slug}`} passHref>
                     <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -63,11 +63,13 @@ function PostDetail({ post }) {
                     </div>
                 </div>
                 <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-                {post.content.raw.children.map((typeObj, index) => {
-                    const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item));
+                <article>
+                    {post.content.raw.children.map((typeObj, index) => {
+                        const children = typeObj.children.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item));
 
-                    return getContentFragment(index, children, typeObj, typeObj.type);
-                })}
+                        return getContentFragment(index, children, typeObj, typeObj.type);
+                    })}
+                </article>
             </div>
         </div>
     );
